5b9f40216e9554020d240e6ea0e224555328d671
Angel Zuniga <angelzuniga@outlook.com>
2020-05-17 07:19:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(array)
   i = 0	  i = 0
   collect = []	  collect = []
-@@ -6,4 +7,6 @@ def my_collect(array)
+
+def my_collect(array)
     i+=1	    i+=1
   end	  
   collect	  collect
